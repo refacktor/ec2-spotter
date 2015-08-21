87d075b1ac2de5e626bed496e0d1b806de505192
root <root@ip-172-31-17-27.ec2.internal>
2015-08-21 00:51:43 +0000

    pivot.sh: Added some error-checking, changed to /dev/sdf1

--- pivot.sh
+++ pivot.sh
@@ -3,21 +3,21 @@
 
 NEWMNT=/new-root
 OLDMNT=old-root
-DEVICE=/dev/xvdj1
+DEVICE=/dev/xvdf1
 
 echo "Mounting $DEVICE ON $NEWMNT"
-mkdir $NEWMNT
-mount $DEVICE $NEWMNT
+[ ! -d $NEWMNT ] && mkdir $NEWMNT
+mount $DEVICE $NEWMNT || exit -1
 
 [ ! -d $NEWMNT/$OLDMNT ] && echo "Creating directory $NEWMNT/$OLDMNT." && mkdir -p $NEWMNT/$OLDMNT
 
 echo "Trying to pivot."
 cd $NEWMNT
-pivot_root . ./$OLDMNT
+pivot_root . ./$OLDMNT || exit -1
 
 for dir in /dev /proc /sys; do
-echo "Moving mounted file system ${OLDMNT}${dir} to $dir."
-mount --move ./${OLDMNT}${dir} ${dir}
+  echo "Moving mounted file system ${OLDMNT}${dir} to $dir."
+  mount --move ./${OLDMNT}${dir} ${dir}
 done
 
 echo "Trying to chroot."
