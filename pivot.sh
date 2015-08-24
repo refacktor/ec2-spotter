9e71d9206694e6e25a2ee879d78367225f255005
root <root@ctcdev>
2015-08-23 20:13:12 -0500

    mount successful across av.zones

--- pivot.sh
+++ pivot.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 export AWS_CREDENTIAL_FILE=/root/.aws.creds
+. /root/.aws.creds
+export AWS_ACCESS_KEY=$AWSAccessKeyId
+export AWS_SECRET_KEY=$AWSSecretKey
 
 export AMIBIN=./ec2-ami-tools-*/bin
 export APIBIN=./ec2-api-tools-*/bin
@@ -10,10 +13,12 @@ export JAVA_HOME=$(echo /usr/lib/jvm/*)
 export ARCH=`uname -i`
 
 export INSTANCE_ID=`curl -s http://169.254.169.254/latest/meta-data/instance-id`
-export AVAIL_ZONE=`curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone`
-export DEST_REGION=${AVAIL_ZONE::-1} # Get the region by stripping the last character of the AVAIL_ZONE
+export DEST_ZONE=`curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone`
+export DEST_REGION=${DEST_ZONE::-1} # Get the region by stripping the last character of the SOURCE_ZONE
 
 SOURCE_REGION=$DEST_REGION # By default, we're in the same region
+#SOURCE_ZONE=$DEST_ZONE
+
 TAG_KEY="Name"
 TAG_VALUE="ctcdev"
 
@@ -27,18 +32,19 @@ TAG_VALUE="ctcdev"
 #fi
 
 echo "Looking for a volume in $SOURCE_REGION with tags ${TAG_KEY} = ${TAG_VALUE}"
-SOURCE_VOLUME=$(${APIBIN}/ec2-describe-tags --region ${SOURCE_REGION} --filter "resource-type=volume" --filter "key=${TAG_KEY}" --filter "value=${TAG_VALUE}" | awk '{print $3}' | head -1)
+volInfo=$(${APIBIN}/ec2-describe-tags --region ${SOURCE_REGION} --filter "resource-type=volume" --filter "key=${TAG_KEY}" --filter "value=${TAG_VALUE}")
+echo $volInfo
+SOURCE_VOLUME=$(echo $volInfo | awk '{print $3}' | head -1)
+SOURCE_ZONE=$(${APIBIN}/ec2-describe-volumes $SOURCE_VOLUME | head -1 | awk '{print $5}')
 
 [[ -z $SOURCE_VOLUME ]] && echo "*** ERROR: No source volume found with tags ${TAG_KEY} = ${TAG_VALUE}" && exit 1
 
-PIVOT_VOLUME=${SOURCE_VOLUME}
-
 # Are we copying the volume from the same region?
-if [[ $SOURCE_REGION != $DEST_REGION ]]; then
+if [[ $SOURCE_ZONE != $DEST_ZONE ]]; then
     # need to copy the volume across
     echo "Volume $SOURCE_VOLUME is in another region"
     echo "Creating a snapshot of the volume"
-    SNAPSHOT=$(${APIBIN}/ec2-create-snapshot --region ${SOURCE_REGION} $SOURCE_VOLUME | awk '{print $2}')
+    SNAPSHOT=$(${APIBIN}/ec2-create-snapshot --region ${SOURCE_REGION} $SOURCE_VOLUME --description 'ec2-spotter temporary snapshot ok to delete' | awk '{print $2}')
     
     echo "Snapshot $SNAPSHOT created. Waiting for completion"
     # Keep checking to see that snapshot has been created
@@ -59,7 +65,7 @@ if [[ $SOURCE_REGION != $DEST_REGION ]]; then
     # Copy the snapshot
     NEW_SNAPSHOT=$(${APIBIN}/ec2-copy-snapshot -r ${SOURCE_REGION} -s ${SNAPSHOT} | awk '{print $2}')
     
-    echo "Copying snapshot from $SOURCE_REGION to $DEST_REGION with name $NEW_SNAPSHOT. Waiting for completion"
+    echo "Copying snapshot from $SOURCE_ZONE to $DEST_ZONE with name $NEW_SNAPSHOT. Waiting for completion"
     
     # Keep checking to see that snapshot has been copied
     count=0
@@ -78,7 +84,7 @@ if [[ $SOURCE_REGION != $DEST_REGION ]]; then
     echo ""
 
     # create volume from this new snapshot
-    NEW_VOLUME=$(${APIBIN}/ec2-create-volume --snapshot ${NEW_SNAPSHOT} -z ${AVAIL_ZONE} | awk '{print $2}')
+    NEW_VOLUME=$(${APIBIN}/ec2-create-volume --snapshot ${NEW_SNAPSHOT} -z ${DEST_ZONE} | awk '{print $2}')
     echo "Creating volume $NEW_VOLUME from $NEW_SNAPSHOT. Waiting for completion"
     
     # Keep checking to see that volume has been created
@@ -98,19 +104,27 @@ if [[ $SOURCE_REGION != $DEST_REGION ]]; then
 
 
     PIVOT_VOLUME=${NEW_VOLUME}
+else
+    PIVOT_VOLUME=${SOURCE_VOLUME}
 fi
 
 echo ""
 echo "Attaching volume $PIVOT_VOLUME as /dev/sdj"
 # Attach volume
-${APIBIN}/ec2-attach-volume $PIVOT_VOLUME -d /dev/sdj --instance $INSTANCE_ID
+${APIBIN}/ec2-attach-volume $PIVOT_VOLUME -d /dev/sdj --instance $INSTANCE_ID || exit -1
+
+while ! lsblk /dev/xvdj1
+do
+  echo "waiting for device to attach"
+  sleep 10
+done
 
 DEVICE=/dev/xvdj1
 NEWMNT=/new-root
 OLDMNT=old-root
 
 echo "Mounting $DEVICE ON $NEWMNT"
-mount $DEVICE $NEWMNT
+mount $DEVICE $NEWMNT || exit -1
 
 [ ! -d $NEWMNT/$OLDMNT ] && echo "Creating directory $NEWMNT/$OLDMNT." && mkdir -p $NEWMNT/$OLDMNT
 
