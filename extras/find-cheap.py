701466c7e683f9da082b887928a7bdb727edcc53
refacktor <277808+refacktor@users.noreply.github.com>
2022-11-04 00:02:58 -0700

    find-cheap.py fixes and improvements

--- extras/find-cheap.py
+++ extras/find-cheap.py
@@ -3,24 +3,38 @@
 # a stand-alone Python script to find the cheapest AWS EC2 Spot Instance types in a region,
 # for a given minimum RAM and CPU
 #
-# usage: find-cheap.py [region] [ram] [cpu]
+# usage: find-cheap.py [ram] [cpu] [regions...]
 
 import sys
 import boto3
 import json
 import pandas as pd
 import math
+import datetime
 
-region = sys.argv[1]
-minMem = float(sys.argv[2])
-minCpu = float(sys.argv[3])
+minMem = float(sys.argv[1])
+minCpu = float(sys.argv[2])
+regions = sys.argv[3:] or ['us-west-1', 'us-west-2', 'us-east-1', 'us-east-2']
 
-client = boto3.client('ec2', region)
-response = client.describe_spot_price_history()
+history = []
 
-sz = pd.read_csv('instance-sizes.csv')
+for region in regions:
+    print('Retrieving price history for %s' % region)
+    client = boto3.client('ec2', region)
+    response = {}
 
-df = pd.DataFrame(response['SpotPriceHistory'])
+    while response == {} or response['NextToken']:
+        response = client.describe_spot_price_history(
+            **response,
+            StartTime=datetime.datetime.now() - datetime.timedelta(hours=1)
+        )
+        history.extend(response['SpotPriceHistory'])
+        response = {'NextToken': response['NextToken']}
+
+sz = pd.read_csv('instance-types.csv')
+
+df = pd.DataFrame(history)
+
 df = df[df.apply(lambda x: not(math.isnan(float(x['SpotPrice']))), axis=1)]
 df['Monthly'] = df['SpotPrice'].astype(float) * 744
 df.drop(['AvailabilityZone'], axis=1)
@@ -29,7 +43,7 @@ df = df.merge(sz, how='outer', on='InstanceType')
 df = df[df.apply(lambda x: x['memory_gb'] >= minMem, axis=1)]
 df = df[df.apply(lambda x: x['vcpu'] >= minCpu, axis=1)]
 df = df.sort_values(by = 'SpotPrice')
-df = df.head(20)
+df = df.head(100)
 
 with pd.option_context('display.max_rows', None):
     print(df)
--- extras/update-instance-types.py
+++ extras/update-instance-types.py
@@ -7,30 +7,22 @@ import requests
 import pandas as pd
 import os.path
 import json
+import sys
 
 if os.path.exists('ec2.json') == False:
-    print("Downloading json file (this will take a very long time)")
+    print("*** IMPORTANT PLEASE READ ***")
     offers = requests.get(
         'https://pricing.us-east-1.amazonaws.com/offers/v1.0/aws/index.json'
     )
     ec2_offer_path = offers.json()['offers']['AmazonEC2']['currentVersionUrl']
-    print(ec2_offer_path)
-    ec2offer = requests.get(    
-        'https://pricing.us-east-1.amazonaws.com%s' % ec2_offer_path
-    ).text()
-    
-    with open('ec2.json', "w") as text_file:
-        text_file.write(ec2offer)
+    print("ec2.json not found. Download it first and then try again:")
+    print("curl https://pricing.us-east-1.amazonaws.com%s > ec2.json" % ec2_offer_path)
+    sys.exit(-1)
 
 else:
-    
+
     with open('ec2.json', 'r') as text_file:
         ec2offer = text_file.read()
-
-#for sku, data in json.loads(ec2offer)['products'].items():
-#    if data['productFamily'] == 'Compute Instance':
-#        new_df = pd.DataFrame.from_dict(data['attributes'], index=[0])
-#        df.append(new_df, ignore_index=True)
 
 data = json.loads(ec2offer)
 
@@ -47,4 +39,4 @@ for product in data['products'].keys():
 df=pd.DataFrame(d).T.reset_index().drop('index',1)
 df.drop_duplicates(inplace=True)
 
-df.to_csv('instance-sizes.csv', index=False)
+df.to_csv('instance-types.csv', index=False)
